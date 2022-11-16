ed298fc7931006b7e2b8a0894e0c1d4fa866414c
Dennis Wang <denniswang@Denniss-MBP.local.meter>
2022-11-16 13:06:50 -0500

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,2 @@
 git config --global --add --bool push.autoSetupRemote true
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install tmux
-brew install fzf
-$(brew --prefix)/opt/fzf/install
-brew install ripgrep
+
